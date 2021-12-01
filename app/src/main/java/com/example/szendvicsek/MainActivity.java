bb5ea8445b841268959baa2702cb31084ebe25ec
Takács Balázs <tbalazs.levente@gmail.com>
2021-12-01 20:52:19 +0100

    Keresés

--- app/src/main/java/com/example/szendvicsek/MainActivity.java
+++ app/src/main/java/com/example/szendvicsek/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.szendvicsek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button main_btnKereses, main_btnUj;
@@ -26,6 +28,42 @@ public class MainActivity extends AppCompatActivity {
                 Intent ujFelvetele = new Intent(MainActivity.this, InsertActivity.class);
                 startActivity(ujFelvetele);
                 finish();
+            }
+        });
+
+        main_btnKereses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ertek = main_editTxtAr.getText().toString().trim();
+                if (!ertek.isEmpty()) {
+                    try {
+                        Cursor adat = db.kereses(ertek);
+                        StringBuilder sb = new StringBuilder();
+                        if (adat.getCount() == 0) {
+                            while (adat.moveToNext()) {
+                                sb.append("Név: ").append(adat.getString(1));
+                                sb.append(System.lineSeparator());
+                                sb.append("Leiras: ").append(adat.getString(2));
+                                sb.append(System.lineSeparator());
+                                sb.append("Elkészítés: ").append(adat.getInt(3));
+                                sb.append(System.lineSeparator());
+                                sb.append("Ár: ").append(adat.getInt(4));
+                                sb.append(System.lineSeparator());
+                                sb.append(System.lineSeparator());
+                            }
+                        } else {
+                            sb.append("Nincs ilyen olcsó szendvics: ").append(ertek);
+                        }
+                        Intent kereses = new Intent(MainActivity.this, SearchResultActivity.class);
+                        String szendvics = db.toString();
+                        kereses.putExtra("szendvics", szendvics);
+                        startActivity(kereses);
+                        finish();
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(getApplicationContext(), "Számot adjon meg",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
