c471e14f1a4828b3f20af86a63ad1653e29e4929
Takács Balázs <tbalazs.levente@gmail.com>
2021-12-01 20:14:51 +0100

    MainActivity.java változók felvétele incializálása

--- app/src/main/java/com/example/szendvicsek/MainActivity.java
+++ app/src/main/java/com/example/szendvicsek/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.szendvicsek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button main_btnKereses, main_btnUj;
+    private EditText main_editTxtAr;
+    private DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+        main_btnKereses = findViewById(R.id.main_btnKereses);
+        main_btnUj = findViewById(R.id.main_btnUj);
+        main_editTxtAr = findViewById(R.id.main_editTxtAr);
+        db = new DBHelper(this);
     }
 }
