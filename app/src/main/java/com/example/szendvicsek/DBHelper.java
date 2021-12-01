5494570d169ab1493288236e67d9c7eef07f015e
Takács Balázs <tbalazs.levente@gmail.com>
2021-12-01 19:32:20 +0100

    DBHelper változók, konstruktor

--- app/src/main/java/com/example/szendvicsek/DBHelper.java
+++ app/src/main/java/com/example/szendvicsek/DBHelper.java
@@ -1,4 +1,32 @@
 package com.example.szendvicsek;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "szendvicsek.db";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME = "szendvicsek";
+    private static final String COL_ID = "id";
+    private static final String COL_NEV = "nev";
+    private static final String COL_LEIRAS = "leiras";
+    private static final String COL_ELKESZITES = "elkeszites";
+    private static final String COL_AR = "ar";
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
